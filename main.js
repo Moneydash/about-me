612926cdfaa4aa85fd34c6ef4363e8a9fee3a985
Christian Raquid <crraquid@gmail.com>
2025-01-27 23:48:05 +0800

    Update
    Update for UI/UX..

--- main.js
+++ main.js
@@ -6,3 +6,20 @@ function smoothScroll(event, targetId) {
         block: 'start'
     });
 }
+
+const backToTopButton = document.getElementById('back-to-top');
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+};
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
